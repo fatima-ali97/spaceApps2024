ee4e7775b4023f5e83b15dcf646c6524631d8d58
fatima-ali97 <fatima.ali.abdulla79@gmail.com>
2024-10-05 14:06:27 +0300

    dashboard fix

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,6 +1,201 @@
-import MainNav from "./mainNav";
+import { useEffect, useState } from "react";
+import Papa from "papaparse";
+import "./App.css";
+import MainNav from "./components/mainNav";
+import Dashboard from "./components/dashboard";
+("./components/mainNav");
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  plugins,
+} from "chart.js";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
+import { Line } from "react-chartjs-2";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 function Dashboard() {
+  const [data, setData] = useState([]);
+  const [country, setCountry] = useState("ALL");
+  const [countries, setCountries] = useState([]);
+  const [experiment, setExperiment] = useState("ALL");
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    fetch("/data.csv")
+      .then((response) => response.text())
+      .then((csv) => {
+        Papa.parse(csv, {
+          header: true,
+          skipEmptyLines: true,
+          complete: (results) => {
+            setData(results.data);
+          },
+        });
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    const codes = [];
+    data.map((row) => {
+      const code = row["Alpha 3 Code"];
+      if (code !== codes[codes.length - 1]) codes.push(code);
+    });
+    setCountries(codes);
+  }, [data]);
+
+  useEffect(() => {
+    if (country === "ALL") {
+      setFilteredData(data);
+    } else {
+      const filter = data.filter((row) => {
+        const code = row["Alpha 3 Code"];
+        return code === country;
+      });
+      setFilteredData(filter);
+    }
+  }, [country]);
+
+  const barData = {
+    labels: ["2015", "2016", "2017", "2018", "2019", "2020"],
+    datasets: [
+      {
+        label: "Rivers CO2 Emissions (TgCO2)",
+        data: filteredData.map((row) => {
+          return row["Rivers (TgCO2)"];
+        }),
+        backgroundColor: "rgba(75, 192, 192, 0.6)",
+      },
+      {
+        label: "Wood + Crop CO2 Emissions (TgCO2)",
+        data: filteredData.map((row) => {
+          return row["Wood+Crop (TgCO2)"];
+        }),
+        backgroundColor: "rgba(153, 102, 255, 0.6)",
+      },
+      {
+        label: "Fossil Fuels CO2 Emissions (TgCO2)",
+        data: filteredData.map((row) => {
+          return row["FF (TgCO2)"];
+        }),
+        backgroundColor: "rgba(255, 99, 132, 0.6)",
+      },
+    ],
+  };
+  const barOptions = {
+    scales: {
+      y: {
+        beginAtZero: true, // Allows negative and positive values
+        ticks: {
+          callback: function (value) {
+            return value + " TgCO2"; // Adds units
+          },
+        },
+        title: {
+          display: true,
+          text: "emissions",
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          text: "year",
+        },
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "CO2 Emissions Sources",
+      },
+    },
+  };
+
+  const Linedata = {
+    labels: [2015, 2016, 2017, 2018, 2019, 2020],
+    datasets: [
+      {
+        label: "IS dC_loss (TgCO2)",
+        data: filteredData.map((row) => {
+          return row["IS dC_loss (TgCO2)"];
+        }),
+        borderColor: "rgb(255, 205, 86)",
+      },
+      {
+        label: "LNLG dC_loss (TgCO2)",
+        data: filteredData.map((row) => {
+          return row["LNLG dC_loss (TgCO2)"];
+        }),
+        borderColor: "rgb(255, 99, 132)",
+      },
+      {
+        label: "LNLGIS dC_loss (TgCO2)",
+        data: filteredData.map((row) => {
+          return row["LNLGIS dC_loss (TgCO2)"];
+        }),
+        borderColor: "rgb(75, 192, 192)",
+      },
+      {
+        label: "LNLGOGIS dC_loss (TgCO2)",
+        data: filteredData.map((row) => {
+          return row["LNLGOGIS dC_loss (TgCO2)"];
+        }),
+        borderColor: "rgb(153, 102, 255)",
+      },
+    ],
+  };
+  const LineOptions = {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: "Time Series of CO2 Emissions (dC_loss) from 2015 to 2020",
+      },
+    },
+    scales: {
+      y: {
+        beginAtZero: false,
+        title: {
+          display: true,
+          text: "dC_loss (TgCO2)",
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          text: "Year",
+        },
+      },
+    },
+  };
+
   return (
     <>
       <h1>
