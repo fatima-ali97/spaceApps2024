9b969d62062c2028efad989ba259d52e1bce1ff5
fatima-ali97 <fatima.ali.abdulla79@gmail.com>
2024-10-05 13:53:47 +0300

    dashboard separation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Papa from "papaparse";
 import "./App.css";
 import MainNav from "./components/mainNav";
+import Dashboard from "./components/dashboard";
 ("./components/mainNav");
 import { Bar } from "react-chartjs-2";
 import {
@@ -200,95 +201,7 @@ function App() {
     <>
       <MainNav></MainNav>
       <div id="dashboard">
-        <h1>
-          Dataset name: Pilot top-down CO2 Budget constrained by the v10 OCO-2
-          MIP Version 1.0
-        </h1>
-
-        <select
-          name="country"
-          id="country"
-          value={country}
-          onChange={(event) => setCountry(event.target.value)}
-        >
-          <option value="ALL">ALL</option>
-          {countries.map((code, index) => {
-            return (
-              <option value={code} key={index}>
-                {code}
-              </option>
-            );
-          })}
-        </select>
-        <br />
-        <select
-          name="experiment"
-          id="experiment"
-          value={experiment}
-          onChange={(event) => setExperiment(event.target.value)}
-        >
-          <option value="ALL">ALL</option>
-          <option value="IS">IS</option>
-          <option value="LNLG">LNLG</option>
-          <option value="LNLGIS">LNLGIS</option>
-          <option value="LNLGOGIS">LNLGOGIS</option>
-        </select>
-        <div>
-          <h2>Raw Data</h2>
-          {data.length > 0 && (
-            <table>
-              <thead>
-                <tr>
-                  {Object.keys(data[0]).map((key, index) => {
-                    if (experiment === "ALL") {
-                      return <th key={index}>{key}</th>;
-                    } else {
-                      const words = key.split(/\s+/);
-                      if (
-                        words.includes(experiment) ||
-                        key === "Alpha 3 Code" ||
-                        key === "Year"
-                      ) {
-                        return <th key={index}>{key}</th>;
-                      }
-                    }
-                  })}
-                </tr>
-              </thead>
-              <tbody>
-                {filteredData.map((row, rowIndex) => (
-                  <tr key={rowIndex}>
-                    {Object.entries(row).map(([property, value], colIndex) => {
-                      if (experiment === "ALL") {
-                        return <td key={colIndex}>{value}</td>;
-                      } else {
-                        const words = property.split(/\s+/); // Split property into words
-                        if (
-                          words.includes(experiment) ||
-                          property === "Alpha 3 Code" ||
-                          property === "Year"
-                        ) {
-                          return <td key={colIndex}>{value}</td>; // Return the column if the condition matches
-                        }
-                        return null; // Skip columns that don't match
-                      }
-                    })}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
-        </div>
-        <div>
-          {country === "ALL" ? (
-            <h2>Please select a country to display charts</h2>
-          ) : (
-            <div>
-              <Bar options={barOptions} data={barData} />
-              <Line options={LineOptions} data={Linedata} />
-            </div>
-          )}
-        </div>
+        <Dashboard></Dashboard>
       </div>
     </>
   );
