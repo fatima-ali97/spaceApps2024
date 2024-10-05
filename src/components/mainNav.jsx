9e7a3e46ab609c9679464022d05a573cab959f79
fatima-ali97 <fatima.ali.abdulla79@gmail.com>
2024-10-05 12:58:58 +0300

    nav list

--- src/components/mainNav.jsx
+++ src/components/mainNav.jsx
@@ -1,11 +1,22 @@
+import dashboard from "../assets/dashboard.svg";
+import team from "../assets/team.svg";
+import references from "../assets/references.svg";
 function MainNav() {
   return (
     <>
       <nav>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+        <ul id="links-list">
+          <li>
+            <img src={dashboard} />
+          </li>
+          <li>
+            {" "}
+            <img src={team} />
+          </li>
+          <li>
+            {" "}
+            <img src={references} />
+          </li>
         </ul>
       </nav>
     </>
